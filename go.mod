8f8c1b74ab66ab5c56ed1335b802688bd3970443
MAfzal <mohamadafzal06@gmail.com>
2023-06-08 18:36:46 +0330

    develop: connecting to minio server via Go:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohamadafzal06/zdrive
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	endpoint := "localhost:9001"
+	accessKeyID := "minio_access_key"
+	secretAccessKey := "minio_secret_key"
+	useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", minioClient) // minioClient is now set up
+}
